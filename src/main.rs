a1f84f7943f10f26de4ade56b6025be6d4ca80b4
Michael Huitt <michael.c.huitt@gmail.com>
2025-10-11 17:19:44 -0400

    Add char by char reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env; //for command line args
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    for arg in args.iter() {
+        arg_parse(arg); 
+    }
+
+}
+
+fn arg_parse(string: &String) {
+   let char_vec: Vec<char> = string.chars().collect();
+
+   for chr in char_vec {
+        println!("{}", chr); 
+   }
+
 }
