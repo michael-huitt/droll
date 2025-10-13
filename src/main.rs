ce440c8ee13f0bf7d5e6323933e5b5f1bd21cefa
Michael Huitt <michael.c.huitt@gmail.com>
2025-10-13 05:02:28 -0400

    Add basic parsing and Err/Ok returns

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,33 @@ use std::env; //for command line args
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    for arg in args.iter() {
+    for arg in args.iter().skip(1) { //skip the first arg since we don't need it
         arg_parse(arg); 
     }
 
 }
 
-fn arg_parse(string: &String) {
-   let char_vec: Vec<char> = string.chars().collect();
+fn arg_parse(string: &String) -> Result<(u32, u32), String> {
+    let char_vec: Vec<char> = string.chars().collect();
+    let mut dflag = false;
+    let mut sflag = false; 
+    let mut die_quant = 0; 
+    let mut die_faces = 0;
 
-   for chr in char_vec {
-        println!("{}", chr); 
-   }
-
+    for chr in char_vec.iter() {
+        if !chr.is_ascii_digit() && (*chr != 'd' && *chr != '*') {
+            return Err(format!("Invalid character: '{}'", chr)); 
+        }
+        
+        if *chr == 'd' {
+            if dflag == true {
+                return Err(format!("Specified 'd' too many times!"));
+            }
+        
+            else {
+                dflag = true;
+            }
+        }
+    }
+    return Ok((die_quant, die_faces));
 }
