802781359c3fdf58592f42f2e925887ecd6b17ee
Michael Huitt <michael.c.huitt@gmail.com>
2025-10-13 05:39:03 -0400

    Implement more robust char handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use std::env; //for command line args
+use std::env; //For command line args
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    for arg in args.iter().skip(1) { //skip the first arg since we don't need it
-        arg_parse(arg); 
+    for arg in args.iter().skip(1) { //Skip the first arg since we don't need it
+        match arg_parse(arg) {
+            Ok((die_quant, die_faces)) => println!("Die: {}\nFaces: {}", die_quant, die_faces),
+            Err(error) => println!("Error: {}", error),
+        } 
     }
 
 }
@@ -13,23 +16,39 @@ fn arg_parse(string: &String) -> Result<(u32, u32), String> {
     let char_vec: Vec<char> = string.chars().collect();
     let mut dflag = false;
     let mut sflag = false; 
-    let mut die_quant = 0; 
-    let mut die_faces = 0;
 
     for chr in char_vec.iter() {
         if !chr.is_ascii_digit() && (*chr != 'd' && *chr != '*') {
             return Err(format!("Invalid character: '{}'", chr)); 
         }
         
-        if *chr == 'd' {
-            if dflag == true {
-                return Err(format!("Specified 'd' too many times!"));
-            }
-        
-            else {
-                dflag = true;
-            }
-        }
+        match *chr {
+            'd' => {
+                if dflag == true {
+                    return Err(format!("Specified 'd' too many times!"));
+                }
+            
+                else {
+                    dflag = true;
+                }
+            },
+            
+            '*' => {
+                if sflag == true {
+                    return Err(format!("Specified '*' too many times!"));
+                }
+            
+                else {
+                    sflag = true;
+                }
+            }, 
+            
+            _ => break,
+        } 
     }
+
+    let mut die_quant = 0; 
+    let mut die_faces = 0;
+    
     return Ok((die_quant, die_faces));
 }
